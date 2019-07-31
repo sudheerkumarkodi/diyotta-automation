8ec6e8e1e6977f763f0e090a19662d6d0c09ddbc
surajsahu8 <surajsahu.nalanda@gmail.com>
2019-07-31 18:20:29 +0530

    Commit Orangehrm packages by Suraj

---
+++ src/test/java/com/orangehrm/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package com.orangehrm.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+
+	}
+
+}
---
+++ src/test/java/com/orangehrm/tests/TestPage.java
@@ -0,0 +1,13 @@
+package com.orangehrm.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestPage {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+
+	}
+
+}
