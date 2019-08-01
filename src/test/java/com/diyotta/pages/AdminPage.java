9c6dcbdb760d168b9b5c84c99b52647ac3efc164
sudheerkumarkodi1 <sudheerkumarkodi1@gmail.com>
2019-08-01 19:27:05 +0530

    path for main module

---
+++ src/test/java/com/diyotta/pages/AdminPage.java
@@ -0,0 +1,13 @@
+package com.diyotta.pages;
+
+import org.openqa.selenium.By;
+
+public class AdminPage {
+
+	public By clickOnMainMenu = By.xpath("//a[@title='DI Suite']");
+	public By studio = By.id("");
+	public By monitor = By.id("");
+	public By scheduler = By.id("");
+	public By lineage = By.id("");
+	public By admin = By.xpath("//li[@id='admin']/a[@title='Admin']/span[0]");
+}
---
+++ src/test/java/com/diyotta/pages/MainMenu.java
@@ -0,0 +1,12 @@
+package com.diyotta.pages;
+
+import org.openqa.selenium.By;
+
+public class MainMenu {
+	public By clickOnMainMenu = By.xpath("//a[@title='DI Suite']");
+	public By studio = By.id("");
+	public By monitor = By.id("");
+	public By scheduler = By.id("");
+	public By lineage = By.id("");
+	public By admin = By.xpath("//li[@id='admin']/a[@title='Admin']/span[0]");
+}
