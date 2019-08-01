ec10df395c38d7cc751ff8a0c2e385ed6b2894d6
surajsahu8 <surajsahu.nalanda@gmail.com>
2019-08-02 02:29:48 +0530

    Changes Committed By Suraj

--- src/test/java/com/diyotta/tests/CommonTestMethods.java
+++ src/test/java/com/diyotta/tests/CommonTestMethods.java
@@ -6,11 +6,13 @@ import org.openqa.selenium.WebElement;
 
 import com.diyotta.constants.TestConstants;
 import com.diyotta.drivers.DriverCreation;
+import com.diyotta.pages.CommonItems;
 import com.diyotta.pages.LoginPage;
 
 public class CommonTestMethods extends DriverCreation {
 
 	LoginPage loginPage = new LoginPage();
+	CommonItems commonItems = new CommonItems();
 
 	public void launchDiyottaAppURL() {
 		createDriver();
@@ -23,12 +25,49 @@ public class CommonTestMethods extends DriverCreation {
 		driver.findElement(loginPage.txtUserName).sendKeys(userName);
 		driver.findElement(loginPage.txtPassword).sendKeys(password);
 		driver.findElement(loginPage.loginButton).click();
+		if (driver.findElement(loginPage.warningPopup).isDisplayed()) {
+			System.out.println("Warning pop up Displayed");
+			// WebElement element = driver.findElement(loginPage.warningContain);
+			// System.out.println(element.getText());
+			// driver.findElement(loginPage.forceLogin).click();
+		} else
+			System.out.println("Warning pop up not present");
+	}
+
+	public String getPageTitle() {
+		String browserTitle = driver.getTitle();
+		return browserTitle;
 	}
 	
-	public String getPageTitle() {
-		String browserTitle=driver.getTitle();
-		return browserTitle;
+	public void AdminModule(String module) {		
+		switch (module.toUpperCase()) {
+		case "STUDIO":
+			driver.findElement(commonItems.moduleMenu).click();
+			driver.findElement(commonItems.moduleAdmin).click();
+			break;
+		case "MONITOR":
+			driver.findElement(commonItems.moduleMenu).click();
+			driver.findElement(commonItems.moduleAdmin).click();
+			break;
+		case "ADMIN":
+			driver.findElement(commonItems.moduleMenu).click();
+			driver.findElement(commonItems.moduleAdmin).click();		
+			break;
+		case "SCHEDULER":
+			driver.findElement(commonItems.moduleMenu).click();
+			driver.findElement(commonItems.moduleAdmin).click();		
+			break;
+		default:
+			System.out.println("Please provide valid module name.");
+			break;
+		}
 		
+	}
+
+	public void logout() {
+		driver.findElement(commonItems.user).click();
+		driver.findElement(commonItems.logout).click();
+
 	}
 
 	public void closeBrowser() {
--- src/test/java/com/diyotta/tests/LoginDiyotta.java
+++ src/test/java/com/diyotta/tests/LoginDiyotta.java
@@ -1,5 +1,6 @@
 package com.diyotta.tests;
 
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import com.diyotta.constants.*;
@@ -10,7 +11,7 @@ public class LoginDiyotta {
 	@BeforeSuite
 	public void openBrowser() {
 		commonTestMethods.launchDiyottaAppURL();
-		if (commonTestMethods.getPageTitle() == "Diyotta ")
+		if (commonTestMethods.getPageTitle().contains("Diyotta"))
 			System.out.println(
 					"Successfully Connect to Diyotta " + TestConstants.TEST_BROWSER.toUpperCase() + " Browser.");
 		else
@@ -18,13 +19,27 @@ public class LoginDiyotta {
 
 	}
 
-	@Test
+	@Test(priority = 0)
 	public void login() {
 		commonTestMethods.login(TestConstants.DI_USERNAME, TestConstants.DI_PASSWORD);
-		if (commonTestMethods.getPageTitle() == " Diyotta | Studio")
-			System.out.println("Successfully Login With "+TestConstants.DI_USERNAME+" user.");
+		if (commonTestMethods.getPageTitle().contains("Diyotta | Studio"))
+			System.out.println("Successfully Login With " + TestConstants.DI_USERNAME + " user.");
 		else
-			System.out.println("Unable to Login to Diyotta");
+			System.out.println("Unable to Login to Diyotta.");
+
+	}
+	@Test(priority = 1, enabled = false)
+	public void admin() {
+		commonTestMethods.AdminModule("ADMIN");
+		System.out.println("Successfully open Admin.");
+	}
+
+	@AfterSuite
+	public void quit() {
+		commonTestMethods.logout();
+		System.out.println("Successfully Logout.");
+		commonTestMethods.closeBrowser();
+		System.out.println("Browser Closed.");
 	}
 
 }
