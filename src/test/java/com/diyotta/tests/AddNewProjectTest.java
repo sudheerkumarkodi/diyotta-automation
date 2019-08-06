8299c15cdef7fdc49c260d3afe8afade4a1effd5
manojkumarjetty <manojkumarjetty@gmail.com>
2019-08-06 19:34:28 +0530

    AddNewProjectTest final

--- src/test/java/com/diyotta/tests/AddNewProjectTest.java
+++ src/test/java/com/diyotta/tests/AddNewProjectTest.java
@@ -1,7 +1,13 @@
 package com.diyotta.tests;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.diyotta.constants.TestConstants;
 import com.diyotta.pages.AddNewProject;
 
 public class AddNewProjectTest {
@@ -9,72 +15,43 @@ public class AddNewProjectTest {
 	AddNewProject addNewProject = new AddNewProject();
 	LoginDiyotta loginDiyotta = new LoginDiyotta();
 	CommonTestMethods commonTestMethods = new CommonTestMethods();
+	
+	@BeforeClass
+	public void openBrowser() {
+		commonTestMethods.launchDiyottaAppURL();
+		System.out.println("We are in @BeforeClass method");
+	}
 
 	@Test(priority = 0)
 	public void addNewProjectMethod() throws InterruptedException {
 
-		loginDiyotta.openBrowser();
-		loginDiyotta.login();
+		System.out.println("We are in @Test method");
+		commonTestMethods.login(TestConstants.DI_USERNAME, TestConstants.DI_PASSWORD);
 		commonTestMethods.getDriver().findElement(addNewProject.diyottaMenus).click();
-
-		loginDiyotta.quit();
-		/*
-		 * commonTestMethods.launchDiyottaAppURL();
-		 * commonTestMethods.login("manoj", "P2wd_4321");
-		 */
-		/*
-		 * loginDiyotta.openBrowser(); loginDiyotta.login();
-		 */
-		/*
-		 * System.setProperty("webdriver.chrome.getDriver()",
-		 * "C:\\Users\\E190402\\Desktop\\Manoj\\Selenium\\diyotta-automation-git\\diyotta-automation\\src\\test\\resources\\drivers\\chromedriver.exe"
-		 * ); getDriver() = new ChromeDriver();
-		 * getDriver().get("http://172.40.9.120:9000/");
-		 * getDriver().manage().window().maximize();
-		 * getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		 * getDriver().findElement(By.id("inputControl_1")).sendKeys("manoj");
-		 * getDriver().findElement(By.id("inputControl_2")).sendKeys("P2wd_4321");
-		 * getDriver().findElement(By.id("inputControl_5")).click();
-		 * 
-		 * addNewProject.setDiyottaMenus(getDriver());
-		 * addNewProject.setAdminMenu(getDriver());
-		 * addNewProject.setProjectMenu(getDriver());
-		 * addNewProject.setNewProjectButton(getDriver());
-		 * addNewProject.setNormalProjectBtn(getDriver());
-		 * addNewProject.setNewProjectName(getDriver(), "Project_Selenium");
-		 * addNewProject.setNewProjectCode(getDriver(), "2662");
-		 * addNewProject.setNewProjectDescription(getDriver(), "Project_Selenium");
-		 * addNewProject.setAddLayers(getDriver()); addNewProject.setLayer(getDriver());
-		 * addNewProject.setNewProjectSaveBtn(getDriver());
-		 * addNewProject.setProjectMenu(getDriver());
-		 */
-		
-		/*
-		 * loginDiyotta.openBrowser(); loginDiyotta.login();
-		 * loginDiyotta.driverInstance().findElement(addNewProject.diyottaMenus).click()
-		 * ; loginDiyotta.driverInstance().findElement(addNewProject.adminMenu).click();
-		 * Thread.sleep(10000);
-		 * loginDiyotta.driverInstance().findElement(addNewProject.projectMenu).click();
-		 * loginDiyotta.driverInstance().findElement(addNewProject.newProjectButton).
-		 * click(); WebElement webElement =
-		 * loginDiyotta.driverInstance().findElement(addNewProject.normalProjectBtn);
-		 * Actions act = new Actions(loginDiyotta.driverInstance());
-		 * act.moveToElement(webElement).perform(); webElement.click();
-		 * loginDiyotta.driverInstance().findElement(addNewProject.newProjectName).
-		 * sendKeys("manoj751");
-		 * loginDiyotta.driverInstance().findElement(addNewProject.newProjectCode).
-		 * sendKeys("manoj751");
-		 * loginDiyotta.driverInstance().findElement(addNewProject.newProjectDescription
-		 * ).sendKeys("manoj751");
-		 * loginDiyotta.driverInstance().findElement(addNewProject.addLayers).click();
-		 * loginDiyotta.driverInstance().findElement(addNewProject.layer).click();
-		 * loginDiyotta.driverInstance().findElement(addNewProject.newProjectSaveBtn).
-		 * click(); loginDiyotta.quit();
-		 * 
-		 * 
-		 * public WebDriver driverInstance() { return commonTestMethods.getDriver(); }
-		 */
-		
+		commonTestMethods.getDriver().findElement(addNewProject.adminMenu).click();
+		Thread.sleep(10000);
+		commonTestMethods.getDriver().findElement(addNewProject.projectMenu).click();
+		commonTestMethods.getDriver().findElement(addNewProject.newProjectButton).click();
+		WebElement webElement = commonTestMethods.getDriver().findElement(addNewProject.normalProjectBtn);
+		Actions act = new Actions(commonTestMethods.getDriver());
+		act.moveToElement(webElement).perform();
+		webElement.click();
+		commonTestMethods.getDriver().findElement(addNewProject.newProjectName).sendKeys(TestConstants.AD_PROJECT_NAME);
+		commonTestMethods.getDriver().findElement(addNewProject.newProjectCode).sendKeys(TestConstants.AD_PROJECT_CODE);
+		commonTestMethods.getDriver().findElement(addNewProject.newProjectDescription).sendKeys(TestConstants.AD_PROJECT_DESC);
+		commonTestMethods.getDriver().findElement(addNewProject.addLayers).click();
+		commonTestMethods.getDriver().findElement(addNewProject.addLayers).click();
+		commonTestMethods.getDriver().findElement(addNewProject.layer).click();
+		commonTestMethods.getDriver().findElement(addNewProject.newProjectSaveBtn).click();
 		
 	}
+
+	@AfterClass
+	public void logout() throws InterruptedException {
+		System.out.println("We are in @AfterClass method");
+		Thread.sleep(5000);
+		commonTestMethods.logout();
+		commonTestMethods.closeBrowser();
+	}
+
 }
