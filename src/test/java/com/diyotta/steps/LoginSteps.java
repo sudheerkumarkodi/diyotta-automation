5a3faa83fec7ff88de6e1e8f97ccc74fcbf5f480
sudheerkumarkodi <ksudheerk.kumar@gmail.com>
2019-08-07 11:08:08 +0530

    cucumber testng migration

--- src/test/java/com/diyotta/steps/LoginSteps.java
+++ src/test/java/com/diyotta/steps/LoginSteps.java
@@ -6,29 +6,49 @@ import io.cucumber.java.en.When;
 
 public class LoginSteps {
 
-	@Given("I am in diyotta login page")
-	public void i_am_in_diyotta_login_page() {
+	@Given("^I am in diyotta login page$")
+	public void i_am_in_diyotta_login_page() throws Throwable {
 		System.out.println("URL");
 	}
 
-	@Then("I enter username {string} and password {string}")
-	public void i_enter_username_and_password(String userName, String password) {
+	@When("^I click on Login button$")
+	public void i_click_on_login_button() throws Throwable {
 		System.out.println("Login");
 	}
 
-	@When("I click on Login button")
-	public void i_click_on_Login_button() {
-		System.out.println("LoginButton");
+	@Then("^I enter username \"([^\"]*)\" and password \"([^\"]*)\"$")
+	public void i_enter_username_something_and_password_something(String strArg1, String strArg2) throws Throwable {
+		System.out.println("username and password");
 	}
 
-	@Then("I should be in home page")
-	public void i_should_be_in_home_page() {
-		System.out.println("Home Page");
+	@Then("^I should be in home page$")
+	public void i_should_be_in_home_page() throws Throwable {
+		System.out.println("home page");
 	}
 
-	@Then("I should see the error message {string}")
-	public void i_should_see_the_error_message(String string) {
-
+	@Then("^I should see the error message \"([^\"]*)\"$")
+	public void i_should_see_the_error_message_something(String errormessage) throws Throwable {
+		System.out.println("error message");
 	}
+
+	/*
+	 * @Given("I am in diyotta login page") public void
+	 * i_am_in_diyotta_login_page() { System.out.println("URL"); }
+	 * 
+	 * @Then("I enter username {string} and password {string}") public void
+	 * i_enter_username_and_password(String userName, String password) {
+	 * System.out.println("Login"); }
+	 * 
+	 * @When("I click on Login button") public void i_click_on_Login_button() {
+	 * System.out.println("LoginButton"); }
+	 * 
+	 * @Then("I should be in home page") public void i_should_be_in_home_page()
+	 * { System.out.println("Home Page"); }
+	 * 
+	 * @Then("I should see the error message {string}") public void
+	 * i_should_see_the_error_message(String string) {
+	 * 
+	 * }
+	 */
 
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,19 +1,39 @@
+@Login
 Feature: Login functionlity
 
-  @Smoke
+  @Smoke @TestSceanrio1
   Scenario: As a user I want to test valid user loging
     Given I am in diyotta login page
     Then I enter username "swetha1223" and password "password123"
     When I click on Login button
     Then I should be in home page
 
-  @Regression
+  @Regression @TestSceanrio2
   Scenario Outline: As a user I want to test inavlid user loging
     Given I am in diyotta login page
     Then I enter username "<username>" and password "<pasword>"
     When I click on Login button
     Then I should see the error message "<errorMessage>"
 
+    Examples: 
+      | username | pasword | errorMessage          |
+      | test123  |     123 | invalid login details |
+      | test123  |         | enter valid password  |
+
+  @TestSceanrio3
+  Scenario Outline: As a user I want to test inavlid user loging-new
+    Given I am in diyotta login page
+    Then I enter username "<username>" and password "<pasword>"
+    When I click on Login button
+    Then I should see the error message "<errorMessage>"
+
+    @Regression
+    Examples: 
+      | username | pasword | errorMessage          |
+      | test123  |     123 | invalid login details |
+      | test123  |         | enter valid password  |
+
+    @Smoke
     Examples: 
       | username | pasword | errorMessage          |
       | test123  |     123 | invalid login details |
