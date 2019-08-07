a6e12da93d7daef9945b8dbb52416453a0894931
sudheerkumarkodi1 <sudheerkumarkodi1@gmail.com>
2019-08-07 08:58:52 +0530

    sudheer

--- src/test/java/com/diyotta/tests/AddNewUser.java
+++ src/test/java/com/diyotta/tests/AddNewUser.java
@@ -1,16 +1,20 @@
 package com.diyotta.tests;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.diyotta.pages.AddNewProject;
 import com.diyotta.pages.MainMenuPage;
+import com.diyotta.pages.UserPage;
 
 
 public class AddNewUser {
 	MainMenuPage mainMenu= new MainMenuPage();
 	LoginDiyotta loginDiyotta = new LoginDiyotta();
 	CommonTestMethods commonTestMethods = new CommonTestMethods();
+	UserPage userPage= new UserPage();
+	
 	
 	@BeforeTest
 	public void login(){
@@ -22,15 +26,13 @@ public class AddNewUser {
 	public void addNewUserMethod() {
 		commonTestMethods.getDriver().findElement(mainMenu.clickOnMainMenu).click();
 		commonTestMethods.getDriver().findElement(mainMenu.admin).click();
-		
-		
+		userPage.addUser();
+		userPage.deleteUser();
 		
 		
 	}
-	
+	@AfterTest
 	public void logOut() {
 		loginDiyotta.quit();
 	}
-	
-
 }
