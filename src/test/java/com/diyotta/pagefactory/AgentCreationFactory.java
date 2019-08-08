b968c2d738f281e7962d1e357ce5da444054126a
leela525 <leelamaale@gmail.com>
2019-08-08 09:49:35 +0530

    BDD agentcreation

--- src/test/java/com/diyotta/pagefactory/AgentCreationFactory.java
+++ src/test/java/com/diyotta/pagefactory/AgentCreationFactory.java
@@ -16,5 +16,29 @@ public class AgentCreationFactory {
 	
 	@FindBy(xpath = "//a[@title='DI Suite']")
 	public WebElement di_Suite;
+	
+	@FindBy(xpath = "//li[@id='admin']")
+	public WebElement admin;
+	
+	@FindBy(xpath = "//li[@title='Expand']")
+	public WebElement expand;
+	
+	@FindBy(xpath = "//span[text()='Agents']")
+	public WebElement agents;
+	
+	@FindBy(xpath = "//span[text()='New']")
+	public WebElement newAgent;
+	
+	@FindBy(id = "inputControl_1")
+	public WebElement agentName;
+	
+	@FindBy(id = "inputControl_4")
+	public WebElement agentHost;
+	
+	@FindBy(id = "inputControl_8")
+	public WebElement agentPort;
+	
+	@FindBy(xpath = "//img[@alt='Save']")
+	public WebElement agentSave;
 
 }
